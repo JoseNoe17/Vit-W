18fa81a958a9b099b49652df28f2d0f491d7cea6
Yarexis Hurtado <yarexis10@gmail.com>
2020-09-02 10:11:23 -0400

    agregado todo el administrador de contenido - VIT

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/*EFECTO MENÚ LATERAL*/
+
+$('.btn-sidebar').on('click', function() {
+    $('.wrapper').toggleClass('collapse-active');
+});
+
+
+$('.btn-sidebar-sm').on('click', function() {
+    $('.wrapper').toggleClass('collapse-active');
+});
+
+
+/*PREVISUALIZAR IMAGEN*/
+
+document.getElementById("customFile").onchange = function(e) {
+    let reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onload = function() {
+        let preview = document.getElementById('preview'),
+        image = document.createElement('img');
+            image.src = reader.result;
+            preview.innerHTML = '';
+            preview.append(image);
+    };
+}
+
+
+document.getElementById("customFileMultiple").onchange = function(e) {
+    let reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onload = function() {
+        let preview = document.getElementById('previewMultiple'),
+        image = document.createElement('img');
+            image.src = reader.result;
+            preview.innerHTML = '';
+            preview.append(image);
+    };
+}
+
+
